29a6350b2e8666fc73f8233615c8d7f825161f85
kunwar45 <knir45@gmail.com>
2021-10-10 15:23:22 -0400

    edited games class to have higher lower game and other changes

--- GameMiniProject_Dice/src/github/kunwar45/Games.java
+++ GameMiniProject_Dice/src/github/kunwar45/Games.java
@@ -1,5 +1,6 @@
 package github.kunwar45;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -23,4 +24,58 @@ public class Games {
 
     }
 
+    public static boolean rollDoubles(int[] dice1, int[] dice2){
+        if (dice1[randomNumber.nextInt(6)] == dice2[randomNumber.nextInt(6)]){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void higherLower(int[] dice){
+
+        ArrayList<Integer> rolls = new ArrayList<Integer>();
+        String guess;
+        String truth;
+        boolean keepPlaying;
+
+        do {
+            rolls.add(dice[randomNumber.nextInt(6)]);
+            System.out.println("The dice roll was: " + rolls.get(rolls.size()-1));
+            System.out.print("What is your guess for the next roll (Higher or Lower) (quit to stop playing)");
+            guess = input.next();
+
+            if (guess.equals("quit")) {
+                keepPlaying = false;
+                break;
+            }
+            else {
+                keepPlaying = true;
+            }
+
+            rolls.add(dice[randomNumber.nextInt(6)]);
+
+            if (rolls.get(rolls.size()-1) == rolls.get(rolls.size()-2)){
+                truth = "lower";
+            }
+            else if (rolls.get(rolls.size()-1) > rolls.get(rolls.size()-2)){
+                truth = "higher";
+            }
+            else if (rolls.get(rolls.size()-1) < rolls.get(rolls.size()-2)){
+                truth = "lower";
+            }
+            else {
+                truth = "nah";
+            }
+
+            if (truth.equals(guess)){
+
+            }
+
+
+        }
+        while (keepPlaying);
+
+    }
 }
