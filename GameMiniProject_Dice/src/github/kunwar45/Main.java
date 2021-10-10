2caf2853ed8a71ca29e75d470aa7eb00430c449d
kunwar45 <knir45@gmail.com>
2021-10-09 22:06:51 -0400

    added initial gui

--- GameMiniProject_Dice/src/github/kunwar45/Main.java
+++ GameMiniProject_Dice/src/github/kunwar45/Main.java
@@ -1,8 +1,21 @@
 package github.kunwar45;
 
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Window myFrame = new Window();
+
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(600, 500); // set frame size
+        myFrame.setVisible(true); // display frame
+
     }
 }
