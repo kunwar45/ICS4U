2f2251ecf05ea9025ee671baa532c4cdf6d63860
kunwar45 <knir45@gmail.com>
2021-10-13 17:46:07 -0400

    added player class

---
+++ GameMiniProject_Dice/src/github/kunwar45/Player.java
@@ -0,0 +1,42 @@
+package github.kunwar45;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    public static String username;
+    public static double initValue;
+    public static ArrayList<Double> historyValues = new ArrayList<>();
+    public static double currentValue;
+
+    public Player(String name, double money){
+        username = name;
+        initValue = money;
+        currentValue = money;
+        historyValues.add(money);
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public double getInitValue(){
+        return initValue;
+    }
+
+    public static ArrayList<Double> getHistoryValues() {
+        return historyValues;
+    }
+
+    public void addValue(double value, boolean won){
+
+        if (won){
+            currentValue += value;
+        }
+        else {
+            currentValue -= value;
+        }
+
+        historyValues.add(currentValue);
+    }
+}
